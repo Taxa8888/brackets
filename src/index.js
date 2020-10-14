aec5eedf0712975de544d634ed9601c6e7dad04a
Taxa8888 <Taxa8888@bk.ru>
2020-10-14 22:25:48 +0300

    feat: add resolution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = bracketsConfig.map(el => el.join(''));
+    for (let i = 0; i < arr.length;) {
+        if (str.includes(arr[i])) {
+            str = str.replace(arr[i], '');
+            i = 0
+        } else {i++}
+    }
+    return !str
 }
+
+// const config1 = [['(', ')']];
+// str = '((()))()(';
+
+// console.log(check(str, config1))
